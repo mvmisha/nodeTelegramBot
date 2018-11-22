130c1b266edf5fd50cb6747fcfe7c501f3139f9d
Mykhaylo Mykhaylov <mishamykhaylov13@gmail.com>
2018-11-22 09:09:04 +0100

    Request to Speedhunters

--- start.js
+++ start.js
@@ -1,2 +1,13 @@
 var config = require('./config.json');
 console.log(config.key);
+
+
+var request = require('request');
+var cheerio = require('cheerio');
+
+request('http://speedhunters.com', function(err, resp, html) {
+        if (!err){
+          const $ = cheerio.load(html);
+          console.log(html); 
+      }
+});
