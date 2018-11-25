27c565db02cc1a5da6d4190b315a28ce09165ea4
Mykhaylo Mykhaylov <mishamykhaylov13@gmail.com>
2018-11-25 14:59:25 +0100

    Fin Domingo

--- start.js
+++ start.js
@@ -5,9 +5,11 @@ console.log(config.key);
 var request = require('request');
 var cheerio = require('cheerio');
 
-request('http://speedhunters.com', function(err, resp, html) {
-        if (!err){
-          const $ = cheerio.load(html);
-          console.log(html); 
-      }
-});
+request('http://speedhunters.com', function (err, resp, html) {
+			if (!err) {
+				const $ = cheerio.load(html);
+				
+				var pepe=$( ".with-image" )[0].children[0];
+				console.log(pepe)
+				}
+			});
