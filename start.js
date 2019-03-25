df8d2f693f8bc37a9ea4be034acaa7446d312f3f
Mykhaylo Mykhaylov <mishamykhaylov13@gmail.com>
2019-03-25 09:34:26 +0100

    Fixes - working in vps

--- start.js
+++ start.js
@@ -1,24 +1,27 @@
-var config = require('./config.json');
-var request = require('request');
-var cheerio = require('cheerio');
-const utf8 = require('utf8');
-
-var urlMensaje = "https://api.telegram.org/bot" + config.key + "/sendPhoto?chat_id=" + config.channel + "&photo=";
-var urlSpeedhunters = 'http://speedhunters.com';
-var src, href, title;
+var config = require('./config.json'),
+	request = require('request'),
+	cheerio = require('cheerio'),
+	utf8 = require('utf8'),
+	fs = require('fs'),
+	urlMensaje = "https://api.telegram.org/bot" + config.key + "/sendPhoto?chat_id=" + config.channel + "&photo=",
+	urlSpeedhunters = 'http://speedhunters.com',
+	src,
+	srcOld=config.photo,
+	href, 
+	title;
 
 function speedhuntersLoad() {
 	request(urlSpeedhunters, function (err, resp, html) {
 		if (!err) {
 			const $ = cheerio.load(html);
 			src = $(".with-image img:first-child").attr('src');
-			if (src != config.article) {
+			if (src != srcOld) {
 				href = $(".with-image a:first-child").attr('href');
 				title = $(".with-image img:first-child").attr('alt');
 				title = utf8.encode(title.replace("&", "and"))
 				urlMensaje = urlMensaje + src + "&caption=" + title + " " + href;
 				sendMessageTelegram(urlMensaje);
-				config.article=src;
+				srcOld=src;
 				fs.writeFileSync('./config.json', JSON.stringify(config));
 			} else {
 				console.log((new Date(Date.now()).toLocaleString()) +" - Message not sent, it's the same article")
@@ -35,7 +38,5 @@ function sendMessageTelegram(urlMensaje) {
 }
 var requestLoop = setInterval(function(){
   speedhuntersLoad();
-}, 600000);
-
-//pruebaforvps
+}, 1800000);
 
